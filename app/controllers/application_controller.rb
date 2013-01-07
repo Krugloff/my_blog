be376b05172e5eb51f03d25adc137935b4134394
Krugloff <mr.krugloff@gmail.com>
2013-01-07 23:26:04 +0400

    Add list of last articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter do
+    @last_articles = Article.last(7) if Article.many?
+  end
+
   private
 
   def search_user
