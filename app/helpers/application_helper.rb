241b07c244e3e6e9b644c881be4aee9cf8721e29
Krugloff <mr.krugloff@gmail.com>
2013-01-08 18:27:20 +0400

    Fix: navigation. 5 hours. Fuck!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def tab_for( content, path )
+    content_tag 'li', link_to( content, path ), class: _active?(path)
+  end
+
+  private
+
+    def _active?(path)
+      current_page?(path) ? 'active' : ''
+    end
 end
