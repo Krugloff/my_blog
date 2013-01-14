b77f5cba2959ef557ccfae08f4d5d42fb6988d1a
Krugloff <mr.krugloff@gmail.com>
2013-01-14 21:48:42 +0400

    Add validate format for user's name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 class User < ActiveRecord::Base
   attr_accessible :name,
     :password,
@@ -9,9 +11,10 @@ class User < ActiveRecord::Base
   has_many :articles
 
   validates :name,
-    length: { maximum: 42 },
+    length: { maximum: 42, minimum: 3 },
     presence: true,
-    uniqueness: { case_sensitive: false }
+    uniqueness: { case_sensitive: false },
+    format: { with: /^[[:word:]\.'` ]+$/i }
 
   def owner?(a_model)
     a_model.user == self
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
@@ -18,25 +20,51 @@ class UserTest < ActiveSupport::TestCase
     assert_nil User.find_by_id(@user.id)
   end
 
-  test "name must be presence" do
+  test "name: must be presence" do
     @user.name = ""
     assert @user.invalid?
   end
 
-  test "name less or equal 42" do
+  test "name: 3 <= length <= 42" do
     @user.name = "?" * 257
+    assert @user.invalid?
+
+    @user.name = 'xx'
     assert @user.invalid?
   end
 
-  test "name must be uniq" do
-    user = User.new( name: "joHn",
+  test "name: must be uniq" do
+    user = User.new( name: "krugloFF",
                      password: "yes",
                      password_confirmation: "yes" )
 
     assert user.invalid?
   end
 
-  test "password must be presence" do
+  test 'name: format' do
+    @user.name = "O'Konnel"
+    assert @user.valid?, @user.name
+
+    @user.name = 'Mr. Dreams'
+    assert @user.valid?, @user.name
+
+    @user.name = 'Симпсон'
+    assert @user.valid?, @user.name
+
+    @user.name = 'Mr, Dreams'
+    assert @user.invalid?, @user.name
+
+    @user.name = ';DROP DATABASE where user.*'
+    assert @user.invalid?, @user.name
+
+    @user.name = "<<\n>>"
+    assert @user.invalid?, @user.name
+
+    @user.name = '>>>>'
+    assert @user.invalid?, @user.name
+  end
+
+  test "password: must be presence" do
     @user.password = ""
     assert @user.invalid?
   end
