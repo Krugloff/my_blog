bd39bb3b136f35619bd3521f4a9d7d37acd0581b
Krugloff <mr.krugloff@gmail.com>
2013-03-13 15:36:41 +0400

    Minimal support omniauth for github

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
+ENV['GITHUB_KEY'] = 'f55a27c565e779ac2d5d'
+ENV['GITHUB_SECRET'] = '71cafb19ca134333c6e2c1452e89f4057ca7870a'
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 MyBlog::Application.initialize!
+
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,11 @@
+#encoding: utf-8
+
+require 'omniauth'
+require 'omniauth-github'
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+end
+
+OmniAuth.config.logger = Rails.logger
+OmniAuth.config.path_prefix = '/sessions'
