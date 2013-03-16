4db75c48994e7e9b3537b13ed1a8de84ed53f88d
Krugloff <mr.krugloff@gmail.com>
2013-03-16 20:15:06 +0400

    Mass-assignment fix

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,6 @@
 class Account < ActiveRecord::Base
+  attr_accessible :uid, :provider
+
   belongs_to :user
 
   validates :uid, :provider, :user_id,
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 #encoding: utf-8
 
 class User < ActiveRecord::Base
+  attr_accessible :name
+
   has_many :articles
   has_many :accounts, :dependent => :destroy
   has_many :comments, :dependent => :destroy
