899545ba542be8152cdae759267605f3c9eb8a66
Krugloff <mr.krugloff@gmail.com>
2013-03-20 12:28:24 +0400

    Move markdown support to /lib

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,14 @@
 require 'redcarpet/krugloff'
 
 class Article < ActiveRecord::Base
+  include MarkdownAsHtml
+
   attr_accessible :title, :body
 
   has_many :comments
   belongs_to :user
 
-  after_save :clear_body_cache, if: '@html'
+  may_be_as_html :body
 
   validates :title,
     presence: true,
@@ -15,20 +17,4 @@ class Article < ActiveRecord::Base
 
   validates :body, :user,
     presence: true
-
-  def body_as_html
-    return @html if @html
-    syntax = Redcarpet::Render::Krugloff.new filter_html: true
-    parser = Redcarpet::Markdown.new syntax,
-      no_intra_emphasis: true,
-      fenced_code_blocks: true,
-      lax_spacing: true
-    @html = parser.render(body).html_safe
-  end
-
-  private
-
-    def clear_body_cache
-      @html = nil
-    end
 end
--- lib/redcarpet/krugloff.rb
+++ lib/redcarpet/krugloff.rb
@@ -7,3 +7,29 @@ module Redcarpet::Render class Krugloff < HTML
     CodeRay.scan( code, language || :text ).div
   end
 end end
+
+module MarkdownAsHtml
+  def self.included(base)
+    base.after_save :clear_body_cache, if: '@html'
+
+    def base.may_be_as_html(*attr_name)
+      attr_name.each do |name|
+        define_method "#{name}_as_html" do
+          return @html if @html
+          syntax = Redcarpet::Render::Krugloff.new filter_html: true
+          parser = Redcarpet::Markdown.new syntax,
+            no_intra_emphasis: true,
+            fenced_code_blocks: true,
+            lax_spacing: true
+          @html = parser.render(self.send name).html_safe
+        end
+      end
+    end
+  end
+
+  private
+
+    def clear_body_cache
+      @html = nil
+    end
+end
