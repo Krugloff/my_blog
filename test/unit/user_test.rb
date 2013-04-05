ee0fce0887e096310d647e546658b353c7c6af6d
Krugloff <mr.krugloff@gmail.com>
2013-04-05 22:09:58 +0400

    Refactoring user test

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,25 +4,25 @@ class UserTest < ActiveSupport::TestCase
   models :users
 
   test 'create' do
-    assert users('admin').persisted?
-    assert users('client').persisted?
+    assert users(:admin).persisted?
+    assert users(:client).persisted?
   end
 
   test 'name must be presence' do
-    assert users('no_name').invalid?
+    assert users(:no_name).invalid?
   end
 
   test 'owner' do
     models :articles, :accounts
 
-    assert users('admin').owner? articles('valid')
-    assert !users('client').owner?( articles 'valid' )
+    assert users(:admin).owner? articles(:valid)
+    assert !users(:client).owner?( articles :valid )
   end
 
   test 'me' do
     models :accounts
 
-    assert users('admin').me?
-    assert !users('client').me?
+    assert users(:admin).me?
+    assert !users(:client).me?
   end
 end
