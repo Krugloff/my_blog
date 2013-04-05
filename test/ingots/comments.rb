86252e53999863805628eaaa85fa9c859d05d426
Krugloff <mr.krugloff@gmail.com>
2013-04-05 17:22:03 +0400

    Refactoring comment test

--- test/ingots/comments.rb
+++ test/ingots/comments.rb
@@ -1,10 +1,11 @@
-@new =  { body: "**Путь программиста** - это осознание существующих абстракций и создание еще более абстрактных абстракций." }
+@new = { body: "**Путь программиста** - это осознание существующих абстракций и создание еще более абстрактных абстракций." }
 
 @invalid_new = @new.merge body: ''
 
-@valid  = @new.merge  user: users('admin'),
-                      article: articles('valid')
+@valid = @new.merge user: users('admin'),
+                    article: articles('valid')
+
+@child = @valid.merge parent_id: 1
 
 @blank_body   = @valid.merge body: ''
 @no_article   = @valid.except :article
-@no_user      = @valid.except :user
--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -3,78 +3,34 @@
 class CommentTest < ActiveSupport::TestCase
   models :comments
 
-  test "create" do
-    ingots 'comments'
-    assert Comment.create comments('valid'), without_protection: true
+  test 'create' do
+    assert comments('valid').persisted?
+    assert comments('child').persisted?
   end
 
-  test "find" do
-    assert Comment.find comments('valid')
-  end
-
-  test "update" do
-    assert comments('valid').update_attribute( :body, "Приветствие" )
-  end
-
-  test "destroy" do
-    comments('valid').destroy
-    assert Comment.where( id: comments('valid').id ).empty?
-  end
-
-  test 'destroy: nested comment' do
-    models 'users', 'articles'
-    child = _nested_comment
-
-    assert_difference( 'Comment.count', 1 ) { child.save }
-    assert_difference( 'Comment.count', -1 ) { comments('valid').destroy }
-    assert_nil Comment.find(child).parent_id,
-      Comment.find(child).parent_id.to_s
-  end
-
-  test "body: must be presence" do
-    assert comments('blank_body').invalid?
-  end
-
-  test 'body_as_html' do
-    assert_no_match %r|```ruby|, comments('valid').body_as_html
-    assert comments('valid').instance_variable_get '@html'
+  test 'body as html' do
     assert_not_equal comments('valid').body, comments('valid').body_as_html
+    assert_no_match %r|```ruby|, comments('valid').body_as_html
     assert_same comments('valid').body_as_html, comments('valid').body_as_html
   end
 
-  test 'body_as_html: cache' do
+  test 'body as html cache' do
     old_value = comments('valid').body_as_html
     comments('valid').update_attribute :body, 'Hello!' # clear cache.
     new_value = comments('valid').body_as_html
+
     assert_not_same old_value, new_value
   end
 
-  test "article: must be presence" do
+  test 'body must be presence' do
+    assert comments('blank_body').invalid?
+  end
+
+  test 'article must be presence' do
     assert comments('no_article').invalid?
   end
 
-  test "user: must be presence" do
-    assert comments('no_user').invalid?
+  test 'user must be presence' do
+    assert comments('new').invalid?
   end
-
-  test 'may be nested' do
-    models 'users', 'articles'
-
-    child = _nested_comment
-
-    assert_difference( "Comment.count", 1, child.inspect ) { child.save }
-
-    assert comments('valid').child_ids.include?( child.id )
-    assert child.parent.persisted?
-  end
-
-  private
-    def _nested_comment
-      child = Comment.new body: comments('new').body,
-        parent_id: comments('valid').id
-
-      child.article = articles('valid')
-      child.user = users('admin')
-      child
-    end
 end
