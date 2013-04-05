d15e33067df4325fdc683c78f81a0bcb09609055
Krugloff <mr.krugloff@gmail.com>
2013-04-05 15:11:48 +0400

    Refactoring account test

--- test/ingots/accounts.rb
+++ test/ingots/accounts.rb
@@ -1,9 +1,8 @@
-@admin =  { uid: '1234567890',
-            provider: 'developer',
-            user: users('admin') }
+@new =  { uid: '1234567890',
+          provider: 'developer' }
 
+@admin  = @new.merge user: users('admin')
 @client = @admin.merge uid: '12345', user: users('client')
 
 @no_uid         = @admin.except :uid
 @blank_provider = @admin.merge provider: ''
-@no_user        = @admin.except :user
--- test/unit/account_test.rb
+++ test/unit/account_test.rb
@@ -1,35 +1,22 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  models 'accounts'
+  models :accounts
 
-  test "create" do
-    ingots 'accounts'
-    assert Account.create accounts('admin'), without_protection: true
+  test 'create' do
+    assert accounts('admin').persisted?
+    assert accounts('client').persisted?
   end
 
-  test "find" do
-    assert Account.find accounts('admin').id
-  end
-
-  test "update" do
-    assert accounts('admin').update_attribute( :provider, "github" )
-  end
-
-  test "destroy" do
-    accounts('admin').destroy
-    assert Account.where( id: accounts('admin').id ).empty?
-  end
-
-  test "provider: must be presence" do
+  test 'provider must be presence' do
     assert accounts('blank_provider').invalid?
   end
 
-  test "user: must be presence" do
-    assert accounts('no_user').invalid?
+  test 'user must be presence' do
+    assert accounts('new').invalid?
   end
 
-  test 'uid: must be presence' do
+  test 'uid must be presence' do
     assert accounts('no_uid').invalid?
   end
 end
