a87bdfa1d57d3fb71f1e1ff4b6f229b6c3ca36df
Krugloff <mr.krugloff@gmail.com>
2013-04-05 22:12:38 +0400

    Refactoring article test

--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -4,35 +4,39 @@ class ArticleTest < ActiveSupport::TestCase
   models :articles
 
   test 'create' do
-    assert articles('valid').valid?
+    assert articles(:valid).valid?
   end
 
   test 'user must be presence' do
-    assert articles('new').invalid?
+    assert articles(:new).invalid?
   end
 
   test 'title must be presence' do
-    assert articles('blank_title').invalid?
+    assert articles(:blank_title).invalid?
   end
 
   test 'title lenght <= 42' do
-    assert articles('big_title').invalid?
+    assert articles(:big_title).invalid?
   end
 
   test 'body must be presence' do
-    assert articles('blank_body').invalid?
+    assert articles(:blank_body).invalid?
   end
 
   test 'body as html' do
-    assert_not_equal articles('valid').body, articles('valid').body_as_html
-    assert_no_match %r|```ruby|, articles('valid').body_as_html
-    assert_same articles('valid').body_as_html, articles('valid').body_as_html
+    article = articles :valid
+
+    assert_not_equal article.body, article.body_as_html
+    assert_no_match %r|```ruby|, article.body_as_html
+    assert_same article.body_as_html, article.body_as_html
   end
 
   test 'body as html: cache' do
-    old_value = articles('valid').body_as_html
-    articles('valid').update_attribute :title, 'Hello!' # clear cache.
-    new_value = articles('valid').body_as_html
+    article = articles :valid
+
+    old_value = article.body_as_html
+    article.update_attribute :title, 'Hello!' # clear cache.
+    new_value = article.body_as_html
 
     assert_not_same old_value, new_value
   end
