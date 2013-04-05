a0941408f0c7dc62fb7ca1696bd5ece1ab1e3972
Krugloff <mr.krugloff@gmail.com>
2013-04-05 16:40:13 +0400

    Refactoring user test

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,37 +3,24 @@
 class UserTest < ActiveSupport::TestCase
   models :users
 
-  test "create" do
-    ingots('users')
-    assert User.create users('admin'), without_protection: true
+  test 'create' do
+    assert users('admin').persisted?
+    assert users('client').persisted?
   end
 
-  test "find" do
-    assert User.find users('admin').id
-  end
-
-  test "update" do
-    assert users('admin').update_attribute( :name, "Mihael" )
-  end
-
-  test "destroy" do
-    users('admin').destroy
-    assert User.where( id: users('admin').id ).empty?
-  end
-
-  test "name: must be presence" do
+  test 'name must be presence' do
     assert users('no_name').invalid?
   end
 
   test 'owner' do
-    models 'articles', 'accounts'
+    models :articles, :accounts
 
     assert users('admin').owner? articles('valid')
     assert !users('client').owner?( articles 'valid' )
   end
 
   test 'me' do
-    models 'accounts'
+    models :accounts
 
     assert users('admin').me?
     assert !users('client').me?
