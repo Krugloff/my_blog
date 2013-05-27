4846da45ab531efecb38c3387e99a023bd9f6f9e
Krugloff <mr.krugloff@gmail.com>
2013-05-27 16:05:44 +0400

    User may destroy accounts.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,6 @@
 class AccountsController < ApplicationController
   def destroy
+    @user.accounts.destroy params[:id]
+    redirect_to user_path
   end
 end
--- test/functional/accounts_controller_test.rb
+++ test/functional/accounts_controller_test.rb
@@ -2,5 +2,15 @@ require 'test_helper'
 
 class AccountsControllerTest < ActionController::TestCase
   test 'destroy' do
+    models :users, :accounts
+
+    login_as :admin
+
+    assert_difference 'Account.count', -1 do
+      delete :destroy, id: accounts(:admin).id
+    end
+
+    assert_response :redirect
+    assert_redirected_to user_path
   end
 end
